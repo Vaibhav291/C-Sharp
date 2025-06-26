076d0f1b6c0c0b64e1f120588783b056258ec39c
Vaibhav29 <vaibhavtonge29@gmail.com>
2025-06-27 00:59:33 +0530

    27/06/2025

--- CShare-project/Program.cs
+++ CShare-project/Program.cs
@@ -20,6 +20,27 @@ class Program
 	
 	//Named Arguments
 	/* PrintDetails(age:27, name: "vaibhav"); */
+	
+	//ref
+	int x = 5;
+	AddTen(ref x);
+	
+	//Params
+	Sum(1,2,3);
+	
+	//Out Parameter
+	int quotient, rem;
+	Divide(10, 3, out quotient, out rem);
+	Console.WriteLine($"Quotient: {quotient}, Remainder: {rem}");
+	
+	//Ref out parameter
+	int number = 10;
+	int result;
+	
+	RefOut(ref number, out result);
+	
+	Console.WriteLine($"Modified Number (ref): {number}");   // Output: 15
+    Console.WriteLine($"Square (out): {result}");  
   }	  
   
   public static void Main()
@@ -40,5 +61,30 @@ class Program
   static void PrintDetails(string name, int age)
   {
 	Console.WriteLine($"{name} - {age}");
+  }
+  
+  public static void AddTen(ref int number)
+  {
+	  number += 10;
+	  Console.WriteLine(number);
+  }
+  
+  public static void Sum(params int[] numbers)
+  {
+	  int total = 0;
+	  foreach (var n in numbers) total += n;
+	  Console.WriteLine(total);
+  }
+  
+  static void Divide(int a, int b, out int result, out int remainder)
+  {
+	  result = a / b;
+	  remainder = a % b;
+  }
+  
+  static void RefOut(ref int value, out int square)
+  {
+	  value += 5;
+	  square = value * value;
   }
 }
